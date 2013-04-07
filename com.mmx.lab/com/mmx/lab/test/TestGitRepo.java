6325ec6c309c53026d84c9c4651ad9da472ac96f
Marin Hadzhiev <mmx_system@yahoo.com>
2013-04-07 14:46:35 +0300

    Test commit with com.mmx.lab project

---
+++ com.mmx.lab/com/mmx/lab/test/TestGitRepo.java
@@ -0,0 +1,11 @@
+package com.mmx.lab.test;
+
+public class TestGitRepo {
+
+	public static void main(String[] args) {
+		final String myName = "Marin Hadzhiev";
+		System.out.println("my name is: " + myName);
+
+	}
+
+}
